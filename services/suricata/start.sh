0e4782e2706470dbf65ae9275813ed90d45ac67b
mohamed <e2044725@ceng.metu.edu.tr>
2023-05-27 17:15:32 +0300

    changed to the final form

--- services/suricata/start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-service suricata start
-sleep 5
-telegraf --config /etc/telegraf/telegraf.conf
--- src/gru-lstm-engine/myapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("predict/temperature", views.predict_temperature, name="predict/temperature"),path("predict/humidity", views.predict_humidity, name="predict/humidity"),
-    path("predict/light", views.predict_light, name="predict/light"),path("predict/co2", views.predict_co2, name="predict/co2"),
-    path("predict/occupancy", views.predict_occupancy, name="predict/occupancy"),
-]
